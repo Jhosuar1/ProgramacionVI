7d83f3ce7c90783263edd37035d6dd6e6bc24ddb
Jhosuar1 <150991515+Jhosuar1@users.noreply.github.com>
2024-04-15 20:14:14 -0500

    Delete parcial 2 parte 1/Components directory

--- parcial 2 parte 1/Components/Model/Control_Fertilizantes.py
+++
@@ -1,6 +0,0 @@
-from .ProductoControl import ProductoControl
-
-class ControlFertilizantes(ProductoControl):
-    def __init__(self, registro_ICA, nombre, frecuencia_aplicacion, valor, fecha_ultima_aplicacion):
-        super().__init__(registro_ICA, nombre, frecuencia_aplicacion, valor)
-        self.fecha_ultima_aplicacion = fecha_ultima_aplicacion
--- parcial 2 parte 1/Components/Model/Control_Plagas.py
+++
@@ -1,6 +0,0 @@
-from .ProductoControl import ProductoControl
-
-class ControlPlagas(ProductoControl):
-    def __init__(self, registro_ICA, nombre, frecuencia_aplicacion, valor, periodo_carencia):
-        super().__init__(registro_ICA, nombre, frecuencia_aplicacion, valor)
-        self.periodo_carencia = periodo_carencia
