19342b8e8cd69b03be49986bbec9b531a934e108
Jhosuar1 <150991515+Jhosuar1@users.noreply.github.com>
2024-04-16 08:33:59 -0500

    Delete Proyecto/Components directory

--- Proyecto/Components/Test/test_Cliente.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Components.Model.Cliente import Cliente
-
-class TestCliente(unittest.TestCase):
-    def test_nombre(self):
-        cliente = Cliente("Juan Pérez", "123456789")
-        self.assertEqual(cliente.nombre, "Juan Pérez")
-
-    # Agrega más pruebas para otros métodos y casos de uso
