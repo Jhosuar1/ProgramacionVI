d7fc86a3ff28d7f534b33b57930e351d16ea27fa
Jhosuar1 <150991515+Jhosuar1@users.noreply.github.com>
2024-05-07 13:27:18 -0500

    Update Factura.py

--- Proyecto/Components/Model/Factura.py
+++ Proyecto/Components/Model/Factura.py
@@ -1,32 +1,32 @@
 class Factura:
-    def __init__(self, fecha, valor_total):# Inicializa una instancia de Factura con una fecha y un valor total
-        self.__fecha = fecha  # Asigna la fecha a un atributo privado
-        self.__valor_total = valor_total  # Asigna el valor total a un atributo privado
-        self.__productos_comprados = []  # Inicializa una lista vacía para almacenar los productos comprados
+    def __init__(self, fecha, valor_total):
+        self.__fecha = fecha  
+        self.__valor_total = valor_total  
+        self.__productos_comprados = [] 
 
     @property
     def fecha(self):
-        return self.__fecha # Método getter para obtener la fecha de la factura
+        return self.__fecha 
 
     @fecha.setter
     def fecha(self, fecha):
-        self.__fecha = fecha # Método setter para actualizar la fecha de la factura
+        self.__fecha = fecha 
         
 
     @property
     def valor_total(self):
-        return self.__valor_total # Método getter para obtener el valor total de la factura
+        return self.__valor_total 
         
 
     @valor_total.setter
     def valor_total(self, valor_total):
-        self.__valor_total = valor_total # Método setter para actualizar el valor total de la factura
+        self.__valor_total = valor_total 
 
     def agregar_producto(self, producto):
-        self.__productos_comprados.append(producto) # Método para agregar un producto a la lista de productos comprados
+        self.__productos_comprados.append(producto)
 
     def obtener_productos(self):
-        return self.__productos_comprados# Método para obtener la lista de productos comprados
+        return self.__productos_comprados
         
 
     
